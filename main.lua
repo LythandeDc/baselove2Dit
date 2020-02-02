09ef7c18288f1fdc70da885f8954fe9da258f0b1
Lythande DC Lister <lythande_gamespeed@yahoo.fr>
2020-02-02 11:42:27 +0100

    main

---
+++ main.lua
@@ -0,0 +1,29 @@
+-- Questa riga mostra le tracce nella console durante l'esecuzione
+io.stdout:setvbuf('no')
+
+-- Impedisce a Love di filtrare i contorni delle immagini quando vengono ridimensionate
+-- Indispensabile per la pixel art
+love.graphics.setDefaultFilter("nearest")
+
+-- Questa riga ti consente di eseguire il debug passo dopo passo in ZeroBraneStudio
+if arg[#arg] == "-debug" then require("mobdebug").start() end
+
+-- LOAD
+function love.load()
+  height = love.graphics.getWidth()
+  width = love.graphics.getHeight()
+end -- END LOAD
+
+-- UPDATE
+function love.update(dt)
+
+end -- END UPDATE
+
+-- DRAW
+function love.draw()
+    
+end -- END DRAW
+
+function love.keypressed(key)
+  print(key)
+end
